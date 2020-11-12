56a0dd58a2bdfc3d6935aaf6f0a63d369f2bbccb
Daniil <hlistunovd@yandex.ru>
2020-11-12 15:03:24 +0300

    Создал функцию обработки данных

--- src/generator/views.py
+++ src/generator/views.py
@@ -1,15 +1,43 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
+
+def run_generator(arg1, arg2):
+    # Пробуем преобразовать строки в числа
+    try:
+        interval = int(arg1)
+        col_steps = int(arg2)
+        # Обрабатываем данные
+        # ...
+        # ...
+
+        # strOut возвращает результат в терминал
+        strOut = f"{interval + col_steps}, '<- Your answer!'"
+        return strOut
+
+    except ValueError:
+        # Выдаем ошибку, в случае неверного ввода данных
+        print('Ввод только чисел!')
+
+    return None
+
+    
 class SendData(View):
+    message_succ = 'Данные успешно отправлены!'
+    message_error = 'данные введены неверно'
+
     def get(self, request):
         return render(request, template_name='index.html', context=None)
 
     def post(self, request):
         col_steps = request.POST['col_steps']
-        print(col_steps)
         interval = request.POST['interval']
-        print(interval)
+        if run_generator(col_steps, interval) == None:
+            messages.error(request, self.message_error)
+        else:
+            run_generator(col_steps, interval)
+            messages.success(request, self.message_succ)
         return redirect('generator:index')
 
 
